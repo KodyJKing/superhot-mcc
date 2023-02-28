53c107ef64ec6d072bff08e65eed0b37d2eb911a
KJK <kody.j.king@gmail.com>
2023-02-27 16:18:46 -0800

    Changed ESP to RSP in scanStack debug script.

--- lua/debuggerscripts.lua
+++ lua/debuggerscripts.lua
@@ -15,10 +15,22 @@ end
 
 function scanRegisters(query)
     local registers = {
-        RAX = RAX, RBX = RBX, RCX = RCX, RDX = RDX,
-        RSI = RSI, RDI = RDI, RBP = RBP, RSP = RSP,
-        R8 = R8, R9 = R9, R10 = R10, R11 = R11,
-        R12 = R12, R13 = R13, R14 = R14, R15 = R15
+        RAX = RAX,
+        RBX = RBX,
+        RCX = RCX,
+        RDX = RDX,
+        RSI = RSI,
+        RDI = RDI,
+        RBP = RBP,
+        RSP = RSP,
+        R8 = R8,
+        R9 = R9,
+        R10 = R10,
+        R11 = R11,
+        R12 = R12,
+        R13 = R13,
+        R14 = R14,
+        R15 = R15
     }
     for name, value in pairs(registers) do
         if value == query then
@@ -31,7 +43,7 @@ end
 
 function scanStack(query, maxOffset, stride)
     for offset = 0, maxOffset, stride do
-        local value = readQword(ESP + offset)
+        local value = readQword(RSP + offset)
         if value == query then
             print("Found value at offset " .. hex(offset))
             return true
