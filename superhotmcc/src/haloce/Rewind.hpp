971221e1c09637cad9da9ec0b2798da243925ff1
KJK <kody.j.king@gmail.com>
2024-05-31 18:56:21 -0700

    removed Rewind::snapshot from header

--- superhotmcc/src/haloce/Rewind.hpp
+++ superhotmcc/src/haloce/Rewind.hpp
@@ -2,6 +2,5 @@
 #include "Halo1.hpp"
 
 namespace Rewind {
-    void snapshot( Halo1::EntityRecord* rec, Halo1::Entity& snap );
     void rewind( Halo1::EntityRecord* rec, float timescale, float globalTimescale, Halo1::Entity& snap );
 }
